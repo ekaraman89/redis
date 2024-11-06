94774979a91610764ab2c1c3ad99c51792b5fa9c
emrah.karaman <emrah.karaman@consultant.boyner.com.tr>
2024-11-06 22:28:41 +0300

    init

---
+++ Redis/Program.cs
@@ -0,0 +1,18 @@
+﻿using StackExchange.Redis;
+
+Console.WriteLine("Hello, World!");
+
+var redis = await ConnectionMultiplexer.ConnectAsync("localhost");
+var db = redis.GetDatabase();
+var subscriber = redis.GetSubscriber();
+
+await subscriber.SubscribeAsync("*", async (channel, key) =>
+{
+    Console.WriteLine($"channel: {channel}, Anahtar değişti: {key}");
+    //
+    // var value = await db.StringGetAsync(new RedisKey(key));
+    // Console.WriteLine($"Anahtarın değeri: {value}");
+});
+
+Console.WriteLine("Redis değişikliklerini dinliyorum...");
+Console.ReadLine(); 
